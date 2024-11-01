7b64496b4222a549bd8516f5e736b1652b7a2183
ni <piligrim.a3@gmail.com>
2024-11-01 16:24:33 +0900

    fix: version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PioneerRpc.Mixfile do
   def project do
     [
       app: :pioneer_rpc,
-      version: "0.1.5.1",
+      version: "0.1.6",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
